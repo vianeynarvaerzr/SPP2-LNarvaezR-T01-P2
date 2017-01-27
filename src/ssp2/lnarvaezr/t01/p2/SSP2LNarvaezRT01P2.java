ea8042233e0de9514e1f92b8bf21aa7fe9661e10
Vianey_Narváez <Vianey_Narvez@192.168.1.70>
2017-01-27 07:53:29 -0600

    Leticia Vianey Narvaez Reyes A01410432 Ingenieria industrial y de sistemas Tarea 01. Problema 3

--- src/ssp2/lnarvaezr/t01/p2/SSP2LNarvaezRT01P2.java
+++ src/ssp2/lnarvaezr/t01/p2/SSP2LNarvaezRT01P2.java
@@ -1,10 +1,10 @@
-/*
+/*//Leticia Vianey Narvaez Reyes A01410432 Ingenieria Industrial y de Sistemas
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package ssp2.lnarvaezr.t01.p2;
-
+import java.util.Scanner;
 /**
  *
  * @author Vianey_Narváez
@@ -15,7 +15,20 @@ public class SSP2LNarvaezRT01P2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    double sup, per, base, altura;
+    Scanner teclado = new Scanner(System.in);
+    
+    System.out.println("Ingrese la base");
+    base = teclado.nextDouble();
+    System.out.println("Ingrase la altura");
+    altura = teclado.nextDouble();
+    
+    per = base+base+altura+altura;
+    sup = base*altura; 
+    
+    System.out.println("El perimetro del rectungulo es " + per);
+    System.out.println("La superficie del rectangulo es " + sup);
+            
     }
     
 }
